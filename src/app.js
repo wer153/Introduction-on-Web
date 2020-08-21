9e41272b7418812a8ff6f5b42308760d736093d0
Kim Minki <77wer153@gmail.com>
2020-08-21 12:49:27 +0900

    deviding index into multiple tabs

--- src/app.js
+++ src/app.js
@@ -21,82 +21,17 @@ app.use(express.static(publicDirectoryPath))
 
 app.get("", (req,res) =>
 {
-  res.render('index',
-  {
-    title: "Weather App",
-    name: "Minki"
-  })
+  res.render('index',{})
 })
 
-app.get("/about", (req, res) =>
+app.get("/portfolio", (req, res) =>
 {
-  res.render("about",
-  {
-    title: "About me",
-    name: "Minki"
-  })
+  res.render("portfolio",{})
 })
 
-app.get("/help", (req, res) =>
+app.get("/contact", (req, res) =>
 {
-  res.render("help",
-  {
-    helpText: "This is some helpful text.",
-    title: "Help",
-    name: "Minki"
-  })
-})
-
-app.get('/weather', (req, res) =>
-{
-  if(!req.query.address)
-  {
-    return res.send
-    ({
-      error: "You must provide an address"
-    })
-  }
-
-
-  geocode(req.query.address, (error, {latitude, longitude, location} = {}) =>
-  {
-    if(error)
-    { return res.send({ error }) }
-
-    forecast(latitude, longitude, (error, forecastData) =>
-    {
-      if(error)
-      { return res.send({ error }) }
-
-      res.send
-      ({
-        forecast: forecastData,
-        location,
-        address: req.query.address
-      })
-    })
-  })
-})
-
-
-app.get('/products', (req, res) =>
-{
-  if (!req.query.search)
-  {
-    return res.send
-    ({
-      error: "You must provide a search term"
-    })
-  }
-
-  res.send
-  ({
-    products: []
-  })
-})
-app.get('/help/*', (req, res) =>
-{
-  res.send("Help article not found")
+  res.render("contact",{})
 })
 
 app.get('*', (req, res) =>
